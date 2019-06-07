f2a089444e231bcc267c97fbd79783a29eda5d6d
lraton <50119007+lraton@users.noreply.github.com>
2019-06-07 08:55:38 +0200

    Create Form1.Designer.cs

---
+++ Software/We+/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace ProvaLoccioni
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tmr = new System.Windows.Forms.Timer(this.components);
+            this.serial = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tmr
+            // 
+            this.tmr.Enabled = true;
+            this.tmr.Interval = 4000;
+            // 
+            // serial
+            // 
+            this.serial.Location = new System.Drawing.Point(505, 9);
+            this.serial.Name = "serial";
+            this.serial.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.serial.Size = new System.Drawing.Size(271, 135);
+            this.serial.TabIndex = 5;
+            this.serial.Text = "Label1";
+            this.serial.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.serial);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        private void btnObjPres_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer tmr;
+        private System.Windows.Forms.Label serial;
+    }
+}
+
