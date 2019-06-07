1e76996aef46cc003f4651b62565b102e78e48d4
lraton <50119007+lraton@users.noreply.github.com>
2019-06-07 08:55:23 +0200

    Create Form1.cs

---
+++ Software/We+/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProvaLoccioni
+{
+    public partial class Form1 : Form
+    {
+
+        Tsk tsk= new Tsk();
+        String msg = tsk.Split();
+        public Form1()
+        {
+            serial.Text=msg;
+            InitializeComponent();
+        }
+
+    }
+}
